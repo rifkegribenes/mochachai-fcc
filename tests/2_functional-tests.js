32e409b0ef3b8d752a51f98f16a669bc43c85527
rifkegribenes <rifkegribenes@gmail.com>
2018-09-27 04:57:26 +0000

    🐣🚀 Checkpoint ./tests/2_functional-tests.js:417995/689

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -166,7 +166,7 @@ suite('Functional Tests', function() {
 
   // On Gomix we'll use this setting
   /** ### Copy your project's url here  ### **/
-  Browser.site = 'https://sincere-cone.gomix.me'; 
+  Browser.site = 'https://rifkegribenes-mochachai-fcc.glitch.me/'; 
   
   // If you are testing on a local environment replace the line above  with 
   // Browser.localhost('example.com', (process.env.PORT || 3000));
@@ -246,14 +246,13 @@ suite('Functional Tests', function() {
             // pressButton is Async.  Waits for the ajax call to complete...
 
             // assert that status is OK 200
-
+            browser.assert.success();
             // assert that the text inside the element 'span#name' is 'Marco'
-
+            browser.assert.text('span#name', 'Cristoforo');
             // assert that the text inside the element 'span#surname' is 'Polo'
-
+            browser.assert.text('span#surname', 'Colombo');
             // assert that the element(s) 'span#dates' exist and their count is 1
-            
-            assert.fail();
+            browser.assert.element('span#dates', 1);
             
             done();   // It's an async test, so we have to call 'done()''
           });
@@ -264,14 +263,21 @@ suite('Functional Tests', function() {
       test('submit "surname" : "Vespucci" - write your e2e test...', function(done) {
 
         // fill the form, and submit.
+        browser
+          .fill('surname', 'Vespucci')
+          .pressButton('submit', function(){
         // assert that status is OK 200
+          browser.assert.success();
         // assert that the text inside the element 'span#name' is 'Amerigo'
+          browser.assert.text('span#name', 'Amerigo');
         // assert that the text inside the element 'span#surname' is 'Vespucci'
+          browser.assert.text('span#surname', 'Vespucci');
         // assert that the element(s) 'span#dates' exist and their count is 1
-        assert.fail();
+          browser.assert.element('span#dates', 1);
         done();
       
       });
     });
   });
 });
+  });
